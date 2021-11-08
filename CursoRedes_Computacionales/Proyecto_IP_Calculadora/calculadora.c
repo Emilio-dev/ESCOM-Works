e685e826b348da6c62d3486cab6a3682025c67cb
Emilio-dev <efloresc1602@alumno.ipn.mx>
2021-11-08 13:56:14 -0600

    Update

--- CursoRedes_Computacionales/Proyecto_IP_Calculadora/calculadora.c
+++ CursoRedes_Computacionales/Proyecto_IP_Calculadora/calculadora.c
@@ -106,11 +106,11 @@ void CalcularClaseMascara()
             IP = conversor(IP);
             menu('c', IP);
         }
-        else if(IP.primero > 223 && IP.primero < 240)
+        else if (IP.primero > 223 && IP.primero < 240)
         {
             printf("IP Clase D\n");
         }
-        else if(IP.primero > 239 && IP.primero < 256)
+        else if (IP.primero > 239 && IP.primero < 256)
         {
             printf("IP Clase E\n");
         }
@@ -204,7 +204,6 @@ void Calcular(char clase, int nedh, int sub, int p, IP IP)
         hosts -= i;
         nedh = (pow(2, i) - 2);
         IP.mask = p;
-
     }
     printf("Nuevos datos\n");
     IP = conversor(IP);
@@ -223,7 +222,7 @@ void Calcular(char clase, int nedh, int sub, int p, IP IP)
 
 void CalcularSubRedes(IP Ip, int sub, int hosts)
 {
-    int i, final = 7, octeto = 1, x, a=0,b=0,j,c=0,d=0;
+    int i, final = 7, octeto = 1, x, a = 0, b = 0, j, c = 0, d = 0;
     for (i = 0; i <= 31; i++)
     {
         if (Ip.mascara[i] == 0)
@@ -240,133 +239,140 @@ void CalcularSubRedes(IP Ip, int sub, int hosts)
     x++;
     if (x < sub)
     {
-    IP *IP_List = ((IP *)malloc(sizeof(IP) * sub));
-    printf("Cantidad de SubRedes:%d,Incremento:2^%d,Octeto a incrementar:%d,hosts por subred:%d\n", sub, final, octeto, hosts);
-    if (octeto == 2)
-    {
-        a = Ip.segundo;
-        for (i = 1; i < sub; i++)
+        IP *IP_List = ((IP *)malloc(sizeof(IP) * sub));
+        printf("Cantidad de SubRedes:%d,Incremento:2^%d,Octeto a incrementar:%d,hosts por subred:%d\n", sub, final, octeto, hosts);
+        if (octeto == 2)
         {
-            IP_List[i].primero = Ip.primero+b;
-            IP_List[i].segundo = a;
-            IP_List[i].tercero = Ip.tercero;
-            IP_List[i].cuarto = Ip.cuarto;
-            a += IP_List[i-1].segundo + pow(2, final);
-             if(a>=255){
-                a = Ip.segundo;
-                b++;
-            }
-            if(b>=255){
-               b = 0;
-               c++; 
+            a = Ip.segundo;
+            for (i = 1; i < sub; i++)
+            {
+                IP_List[i].primero = Ip.primero + b;
+                IP_List[i].segundo = a;
+                IP_List[i].tercero = Ip.tercero;
+                IP_List[i].cuarto = Ip.cuarto;
+                a += IP_List[i - 1].segundo + pow(2, final);
+                if (a >= 255)
+                {
+                    a = Ip.segundo;
+                    b++;
+                }
+                if (b >= 255)
+                {
+                    b = 0;
+                    c++;
+                }
             }
         }
-    }
-    if (octeto == 3)
-    {
-        a = Ip.tercero;
-       
-        for (i = 0; i < sub; i++)
+        if (octeto == 3)
         {
-            IP_List[i].primero = Ip.primero+c;
-            IP_List[i].segundo = Ip.segundo+b;
-            IP_List[i].tercero = a;
-            IP_List[i].cuarto = Ip.cuarto;
-            a += IP_List[i-1].tercero + pow(2, final);
-             if(a>=255){
-                a = Ip.tercero;
-                b++;
-            }
-            if(b>=255){
-               b = 0;
-               c++; 
-            }
-        }
-    }
-    if (octeto == 4)
-    {
-        a = Ip.cuarto;
-        for (i = 1; i < sub; i++)
-        {
-            IP_List[i].primero = Ip.primero+d;
-            IP_List[i].segundo = Ip.segundo+c;
-            IP_List[i].tercero = Ip.tercero+b;
-            IP_List[i].cuarto =  a;
-            a += IP_List[i-1].cuarto + pow(2, final);
-            if(a>=255){
-                a = Ip.cuarto;
-                b++;
-            }
-            if(b>=255){
-               b = 0;
-               c++; 
-            }
-            if(c>=255){
-               c = 0;
-               d++; 
-            }
-        }
-    }
+            a = Ip.tercero;
 
-    for (i = 0; i < sub; i++)
-        printf("%d.%d.%d.%d\n", IP_List[i].primero, IP_List[i].segundo, IP_List[i].tercero, IP_List[i].cuarto);
-    ListaHosts(IP_List, hosts, octeto, x);
+            for (i = 0; i < sub; i++)
+            {
+                IP_List[i].primero = Ip.primero + c;
+                IP_List[i].segundo = Ip.segundo + b;
+                IP_List[i].tercero = a;
+                IP_List[i].cuarto = Ip.cuarto;
+                a += IP_List[i - 1].tercero + pow(2, final);
+                if (a >= 255)
+                {
+                    a = Ip.tercero;
+                    b++;
+                }
+                if (b >= 255)
+                {
+                    b = 0;
+                    c++;
+                }
+            }
+        }
+        if (octeto == 4)
+        {
+            a = Ip.cuarto;
+            for (i = 1; i < sub; i++)
+            {
+                IP_List[i].primero = Ip.primero + d;
+                IP_List[i].segundo = Ip.segundo + c;
+                IP_List[i].tercero = Ip.tercero + b;
+                IP_List[i].cuarto = a;
+                a += IP_List[i - 1].cuarto + pow(2, final);
+                if (a >= 255)
+                {
+                    a = Ip.cuarto;
+                    b++;
+                }
+                if (b >= 255)
+                {
+                    b = 0;
+                    c++;
+                }
+                if (c >= 255)
+                {
+                    c = 0;
+                    d++;
+                }
+            }
+        }
+
+        for (i = 0; i < sub; i++)
+            printf("%d.%d.%d.%d\n", IP_List[i].primero, IP_List[i].segundo, IP_List[i].tercero, IP_List[i].cuarto);
+        ListaHosts(IP_List, hosts, octeto, x);
     }
     else
     {
-    IP *IP_List = ((IP *)malloc(sizeof(IP) * x));
-    printf("Cantidad de SubRedes:%d,Incremento:2^%d,Octeto a incrementar:%d,hosts por subred:%d\n", x, final, octeto, hosts);
-    if (octeto == 2)
-    {
-        IP_List[0].primero = Ip.primero;
-        IP_List[0].segundo = Ip.segundo;
-        a = Ip.segundo;
-        IP_List[0].tercero = Ip.tercero;
-        IP_List[0].cuarto = Ip.cuarto;
-        for (i = 1; i < x; i++)
+        IP *IP_List = ((IP *)malloc(sizeof(IP) * x));
+        printf("Cantidad de SubRedes:%d,Incremento:2^%d,Octeto a incrementar:%d,hosts por subred:%d\n", x, final, octeto, hosts);
+        if (octeto == 2)
         {
-            IP_List[i].primero = Ip.primero;
-            IP_List[i].segundo = pow(2, final) + a;
-            a += Ip.segundo + pow(2, final);
-            IP_List[i].tercero = Ip.tercero;
-            IP_List[i].cuarto = Ip.cuarto;
+            IP_List[0].primero = Ip.primero;
+            IP_List[0].segundo = Ip.segundo;
+            a = Ip.segundo;
+            IP_List[0].tercero = Ip.tercero;
+            IP_List[0].cuarto = Ip.cuarto;
+            for (i = 1; i < x; i++)
+            {
+                IP_List[i].primero = Ip.primero;
+                IP_List[i].segundo = pow(2, final) + a;
+                a += Ip.segundo + pow(2, final);
+                IP_List[i].tercero = Ip.tercero;
+                IP_List[i].cuarto = Ip.cuarto;
+            }
         }
-    }
-    if (octeto == 3)
-    {
-        IP_List[0].primero = Ip.primero;
-        IP_List[0].segundo = Ip.segundo;
-        IP_List[0].tercero = Ip.tercero;
-        a = Ip.tercero;
-        IP_List[0].cuarto = Ip.cuarto;
-        for (i = 1; i < x; i++)
+        if (octeto == 3)
         {
-            IP_List[i].primero = Ip.primero;
-            IP_List[i].segundo = Ip.segundo;
-            IP_List[i].tercero = pow(2, final) + a;
-            a += Ip.tercero + pow(2, final);
-            IP_List[i].cuarto = Ip.cuarto;
+            IP_List[0].primero = Ip.primero;
+            IP_List[0].segundo = Ip.segundo;
+            IP_List[0].tercero = Ip.tercero;
+            a = Ip.tercero;
+            IP_List[0].cuarto = Ip.cuarto;
+            for (i = 1; i < x; i++)
+            {
+                IP_List[i].primero = Ip.primero;
+                IP_List[i].segundo = Ip.segundo;
+                IP_List[i].tercero = pow(2, final) + a;
+                a += Ip.tercero + pow(2, final);
+                IP_List[i].cuarto = Ip.cuarto;
+            }
         }
-    }
-    if (octeto == 4)
-    {
-        IP_List[0].primero = Ip.primero;
-        IP_List[0].segundo = Ip.segundo;
-        IP_List[0].tercero = Ip.tercero;
-        IP_List[0].cuarto = Ip.cuarto;
-        a = Ip.cuarto;
-        for (i = 1; i < x; i++)
+        if (octeto == 4)
         {
-            IP_List[i].primero = Ip.primero;
-            IP_List[i].segundo = Ip.segundo;
-            IP_List[i].tercero = Ip.tercero;
-            IP_List[i].cuarto = pow(2, final) + a;
-            a += Ip.cuarto + pow(2, final);
+            IP_List[0].primero = Ip.primero;
+            IP_List[0].segundo = Ip.segundo;
+            IP_List[0].tercero = Ip.tercero;
+            IP_List[0].cuarto = Ip.cuarto;
+            a = Ip.cuarto;
+            for (i = 1; i < x; i++)
+            {
+                IP_List[i].primero = Ip.primero;
+                IP_List[i].segundo = Ip.segundo;
+                IP_List[i].tercero = Ip.tercero;
+                IP_List[i].cuarto = pow(2, final) + a;
+                a += Ip.cuarto + pow(2, final);
+            }
         }
-    }
-    for (i = 0; i < x; i++)
-        printf("%d.%d.%d.%d\n", IP_List[i].primero, IP_List[i].segundo, IP_List[i].tercero, IP_List[i].cuarto);
-    ListaHosts(IP_List, hosts, octeto, x);
+        for (i = 0; i < x; i++)
+            printf("%d.%d.%d.%d\n", IP_List[i].primero, IP_List[i].segundo, IP_List[i].tercero, IP_List[i].cuarto);
+        ListaHosts(IP_List, hosts, octeto, x);
     }
 }
 
@@ -413,4 +419,3 @@ void ListaHosts(IP *IP_List, int hosts, int octeto, int x)
     for (i = 0; i < hosts; i++)
         printf("%d.%d.%d.%d\n", IP_Hosts[i].primero, IP_Hosts[i].segundo, IP_Hosts[i].tercero, IP_Hosts[i].cuarto);
 }
-
