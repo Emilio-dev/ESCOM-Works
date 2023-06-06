42f284bfae79e6cbc4f031692aa2ddb05b3fcb0f
Emilio Flores Castañeda <efloresc1602@alumno.ipn.mx>
2023-06-06 12:53:20 -0600

    Reorder of files and renames

--- CursoCompiladores/A_Sintactico_Ascendente_R_Proyecto/prueba.c
+++ CursoCompiladores/A_Sintactico_Ascendente_R_Proyecto/prueba.c
@@ -1,12 +1,12 @@
 /*
 Código para calcula la serie de Fibonacci
 */
-var fib = 0;
+var fib = 0 + 1;
 var lim = 10;
 var aux = 1;
 
 para(var init = 1; init <= lim; init = init + 1){
     imprimir fib;
     aux = aux + fib;
-    fib = aux - fib;
+
 }
